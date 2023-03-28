2834837c5e2e76e8f4467fc017c16893ce5f21f1
2taezeat <xorudfl772@naver.com>
2023-03-28 18:42:42 +0900

    feat: Main activity binding

--- app/src/main/java/com/example/recommendtrack/MainActivity.kt
+++ app/src/main/java/com/example/recommendtrack/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.recommendtrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.recommendtrack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
+
+
 }
