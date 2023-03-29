45117e0ea1ce1fe0d8ffe89a84f634f969c3f03a
2taezeat <xorudfl772@naver.com>
2023-03-29 15:58:23 +0900

    feat: mainactivity navigation 연결

---
+++ app/src/main/java/com/example/recommendtrack/RecommendTrackFragment.kt
@@ -0,0 +1,34 @@
+package com.example.recommendtrack
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RecommendTrackFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recommend_track, container, false)
+    }
+
+    companion object {
+
+        fun newInstance(param1: String, param2: String) =
+            RecommendTrackFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
