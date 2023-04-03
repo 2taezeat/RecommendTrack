5eab9d21b5efe220f0b15b66671c45d3e7669e9e
2taezeat <xorudfl772@naver.com>
2023-04-03 16:19:32 +0900

    feat: BaseFragment 생성

---
+++ app/src/main/java/com/example/recommendtrack/BaseFragment.kt
@@ -0,0 +1,44 @@
+package com.example.recommendtrack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+abstract class BaseFragment<T : ViewDataBinding>(private val layoutId: Int, private val vmClass : Class<ViewModel>) : Fragment() {
+
+    private var _binding: T? = null
+    protected val binding
+        get() = requireNotNull(_binding)
+
+    val viewModel by lazy {
+        ViewModelProvider(this)[vmClass]
+    }
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate<T>(inflater, layoutId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
