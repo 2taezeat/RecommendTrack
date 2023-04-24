05bd66a9f79166153dbc750bcec6ce5ae3df076e
2taezeat <xorudfl772@naver.com>
2023-04-24 14:07:27 +0900

    feat: GenreMapper 변경

---
+++ app/src/main/java/com/example/recommendtrack/data/mapper/GenreMapper.kt
@@ -0,0 +1,18 @@
+package com.example.recommendtrack.data.mapper
+
+import com.example.recommendtrack.data.dto.GenresDto
+import com.example.recommendtrack.data.dto.TokenDto
+import com.example.recommendtrack.domain.entity.Genre
+import com.example.recommendtrack.domain.entity.Token
+import com.skydoves.sandwich.ApiResponse
+import com.skydoves.sandwich.ApiSuccessModelMapper
+
+
+
+
+object GenreMapper : ApiSuccessModelMapper<GenresDto, List<Genre>> {
+    override fun map(apiSuccessResponse: ApiResponse.Success<GenresDto>): List<Genre> {
+        return apiSuccessResponse.data.genres.toList().map { Genre(it) }
+    }
+
+}
