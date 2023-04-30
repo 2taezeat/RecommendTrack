b5cb050a4453780df3acb288f6d4ab5934a14dea
2taezeat <xorudfl772@naver.com>
2023-04-30 20:53:11 +0900

    feat: Genre Entity PK 변경

--- app/src/main/java/com/example/recommendtrack/domain/entity/Genre.kt
+++ app/src/main/java/com/example/recommendtrack/domain/entity/Genre.kt
@@ -2,12 +2,10 @@ package com.example.recommendtrack.domain.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
 
 @Entity
 data class Genre(
-    @PrimaryKey val id: UUID = UUID.randomUUID(),
-    val name: String,
+    @PrimaryKey val name: String,
     var isSelected: Boolean = false
 
 )
