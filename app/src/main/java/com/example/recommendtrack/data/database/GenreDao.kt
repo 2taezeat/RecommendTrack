c533e20b349b672d9c094a503974e4fb2ce8d184
2taezeat <xorudfl772@naver.com>
2023-05-02 16:12:36 +0900

    feat: GenreDao getMyGenres Query 변경

--- app/src/main/java/com/example/recommendtrack/data/database/GenreDao.kt
+++ app/src/main/java/com/example/recommendtrack/data/database/GenreDao.kt
@@ -7,14 +7,21 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface GenreDao {
 
-    @Query("SELECT * FROM genre")
+    @Query("SELECT * FROM genre WHERE isSelected = true")
     fun getMyGenres(): Flow<List<Genre>>
 
-    @Update
-    fun updateMyGenre(genre: Genre)
+
+    @Transaction
+    open suspend fun updateMyGenre(genres: List<Genre>) {
+        addMyGenres(genres)
+        deleteMyGenres(genres)
+    }
+
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addMyGenres(genres: List<Genre>)
 
+    @Delete
+    suspend fun deleteMyGenres(genres: List<Genre>)
 
 }
