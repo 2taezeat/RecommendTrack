d7c265fd4f2a91b72c803b91aa0e091dac1b9736
2taezeat <xorudfl772@naver.com>
2023-05-02 21:39:15 +0900

    feat: ArtistTypeConverters 추가 및 반영

--- app/src/main/java/com/example/recommendtrack/data/database/ArtistDatabase.kt
+++ app/src/main/java/com/example/recommendtrack/data/database/ArtistDatabase.kt
@@ -2,10 +2,14 @@ package com.example.recommendtrack.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.example.recommendtrack.domain.entity.Artist
 
 
 @Database(entities = [Artist::class], version = 1)
+@TypeConverters(ArtistTypeConverters::class)
 abstract class ArtistDatabase : RoomDatabase() {
     abstract fun artistDao(): ArtistDao
 }
+
--- app/src/main/java/com/example/recommendtrack/domain/entity/Artist.kt
+++ app/src/main/java/com/example/recommendtrack/domain/entity/Artist.kt
@@ -1,9 +1,13 @@
 package com.example.recommendtrack.domain.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Artist(
     val followers: Int,
     val genres : List<String>,
-    val id: String,
+    @PrimaryKey val id: String,
     val imageUrl: String,
     val name: String,
     val popularity: Int,
