b667d6bacac4923b31022f40879279d52c550f05
2taezeat <xorudfl772@naver.com>
2023-05-02 18:44:17 +0900

    feat: ArtistApi Query 수정

--- app/src/main/java/com/example/recommendtrack/data/remote/ArtistApi.kt
+++ app/src/main/java/com/example/recommendtrack/data/remote/ArtistApi.kt
@@ -3,16 +3,17 @@ package com.example.recommendtrack.data.remote
 import com.example.recommendtrack.data.dto.ArtistDto
 import com.example.recommendtrack.data.dto.GenresDto
 import com.skydoves.sandwich.ApiResponse
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface ArtistApi {
 
-    @GET("search/")
+    @GET("search")
     suspend fun getArtist(
         @Header("Authorization") accessToken: String,
-        @Query("q=artist%3A") artistName: String,
+        @Query("q") artistName: String,
         @Query("type") type: String = "artist",
         @Query("limit") limit : Int = 1,
         @Query("offset") offset : Int = 0,
--- app/src/main/java/com/example/recommendtrack/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/recommendtrack/presentation/main/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.recommendtrack.domain.usecase.GetTokenUseCase
+import com.example.recommendtrack.domain.usecase.artist.GetArtistUseCase
 import com.example.recommendtrack.utils.Constants.SPOTIFY_CLIENT_ID
 import com.example.recommendtrack.utils.Constants.SPOTIFY_CLIENT_SECRET
 import com.example.recommendtrack.utils.Constants.SPOTIFY_GRANT_TYPE
@@ -13,18 +14,21 @@ import com.example.recommendtrack.utils.PreferenceKey.tokenPreferenceKey
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getTokenUseCase: GetTokenUseCase,
+    private val getArtistUseCase: GetArtistUseCase,
     private val dataStore: DataStore<Preferences>
 ) : ViewModel() {
 
 
     init {
         getToken()
+        getArtist()
     }
 
     private fun tokenFlowFromDataStore(): Flow<String> = dataStore.data.map { preferences ->
@@ -52,6 +56,18 @@ class MainViewModel @Inject constructor(
             }
         }
     }
+
+
+    fun getArtist() {
+        viewModelScope.launch {
+            val accessToken = "Bearer ${tokenFlowFromDataStore().first()}"
+            getArtistUseCase.invoke(accessToken, "artist:2pac").collect { it ->
+                Timber.d("${it}")
+
+            }
+        }
+    }
+
 
 
 }
