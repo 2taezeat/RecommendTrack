c7f805aa5740b8dc35dabcfe78a122b22311691c
2taezeat <xorudfl772@naver.com>
2023-05-02 20:36:34 +0900

    feat: searchArtist binding

--- app/src/main/java/com/example/recommendtrack/presentation/artist/ArtistFragment.kt
+++ app/src/main/java/com/example/recommendtrack/presentation/artist/ArtistFragment.kt
@@ -1,18 +1,26 @@
 package com.example.recommendtrack.presentation.artist
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.SearchView
+import androidx.databinding.adapters.SearchViewBindingAdapter.setOnQueryTextListener
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.recommendtrack.R
 import com.example.recommendtrack.databinding.FragmentArtistBinding
 import com.example.recommendtrack.presentation.ui.BaseFragment
 import com.example.recommendtrack.presentation.ui.artist.ArtistViewModel
-import timber.log.Timber
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class ArtistFragment : BaseFragment<FragmentArtistBinding>(R.layout.fragment_artist) {
     private val viewModel by viewModels<ArtistViewModel>()
+    private lateinit var searchView: SearchView
+    private lateinit var artistNameTextView: TextView
+    private lateinit var artistFollowersTextView: TextView
+    private lateinit var artistPopularityTextView: TextView
+    private lateinit var artistGenresTextView: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +30,50 @@ class ArtistFragment : BaseFragment<FragmentArtistBinding>(R.layout.fragment_art
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Timber.d("${viewModel}")
+        initView()
+
+
+        viewModel.searchArtist.observe(viewLifecycleOwner, Observer { it ->
+            artistNameTextView.text = it.name
+            artistFollowersTextView.text = it.followers.toString()
+            artistPopularityTextView.text = it.popularity.toString()
+            artistGenresTextView.text = it.genres.toString()
+
+        })
+
+
+    }
+
+    private fun initView() {
+        initSearchView()
+        initContentTextView()
+    }
+
+    private fun initContentTextView() {
+        artistNameTextView = binding.artistNameContentTv
+        artistFollowersTextView = binding.artistFollowersContentTv
+        artistPopularityTextView = binding.artistPopularityContentTv
+        artistGenresTextView = binding.artistGenresContentTv
+    }
+
+
+    private fun initSearchView() {
+        searchView = binding.artistSearchView
+        searchView.apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(searchString: String): Boolean {
+                    viewModel.getArtist(searchString)
+                    return true
+                }
+
+                override fun onQueryTextChange(searchString: String): Boolean {
+                    return false
+                }
+            })
+
+        }
+
+
     }
 
 
