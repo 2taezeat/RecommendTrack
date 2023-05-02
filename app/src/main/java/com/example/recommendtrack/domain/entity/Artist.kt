c4015dc65dc50c823651cfdcd5dd85bfaaf67788
2taezeat <xorudfl772@naver.com>
2023-05-02 18:26:08 +0900

    feat: Artist Entity id type 수정

--- app/src/main/java/com/example/recommendtrack/domain/entity/Artist.kt
+++ app/src/main/java/com/example/recommendtrack/domain/entity/Artist.kt
@@ -3,7 +3,7 @@ package com.example.recommendtrack.domain.entity
 data class Artist(
     val followers: Int,
     val genres : List<String>,
-    val id: Int,
+    val id: String,
     val imageUrl: String,
     val name: String,
     val popularity: Int,
