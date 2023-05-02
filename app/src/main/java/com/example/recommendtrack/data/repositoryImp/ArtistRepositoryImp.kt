50cf31f3f796fc5e3cc4fc52fac214b6caa6a192
2taezeat <xorudfl772@naver.com>
2023-05-02 18:25:50 +0900

    feat: get Artist Data layer 구현

---
+++ app/src/main/java/com/example/recommendtrack/data/repositoryImp/ArtistRepositoryImp.kt
@@ -0,0 +1,43 @@
+package com.example.recommendtrack.data.repositoryImp
+
+import com.example.recommendtrack.data.datasource.ArtistRemoteDataSource
+import com.example.recommendtrack.data.mapper.ArtistMapper
+import com.example.recommendtrack.data.mapper.ErrorEnvelopeMapper
+import com.example.recommendtrack.domain.entity.Artist
+import com.example.recommendtrack.domain.repository.ArtistRepository
+import com.skydoves.sandwich.message
+import com.skydoves.sandwich.suspendOnError
+import com.skydoves.sandwich.suspendOnFailure
+import com.skydoves.sandwich.suspendOnSuccess
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import timber.log.Timber
+import javax.inject.Inject
+
+class ArtistRepositoryImp
+@Inject constructor(private val dataSource: ArtistRemoteDataSource, private val ioDispatcher: CoroutineDispatcher) : ArtistRepository {
+
+    override suspend fun fetchArtist(accessToken: String, artistName: String): Flow<Artist> {
+        val response = dataSource.fetchArtist(accessToken, artistName)
+
+        val flowArtist = flow {
+            response.suspendOnSuccess(ArtistMapper) {
+                val artist = this
+                emit(artist)
+                Timber.tag("success").d("$artist")
+            }.suspendOnFailure {
+                Timber.tag("fail").d("${this.message()}")
+            }.suspendOnError(ErrorEnvelopeMapper) {
+                val errorMessage = this.message
+                Timber.tag("error").d("$errorMessage")
+            }
+        }.flowOn(ioDispatcher)
+
+
+        return flowArtist
+    }
+
+
+}
