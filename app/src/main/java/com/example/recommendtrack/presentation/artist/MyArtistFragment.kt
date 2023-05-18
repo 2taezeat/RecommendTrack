3fd6d64d338e577b9d520178f18aea7ffbe40465
2taezeat <xorudfl772@naver.com>
2023-05-18 17:12:49 +0900

    feat : MyArtistFragment 생성

---
+++ app/src/main/java/com/example/recommendtrack/presentation/artist/MyArtistFragment.kt
@@ -0,0 +1,25 @@
+package com.example.recommendtrack.presentation.artist
+
+import android.os.Bundle
+import android.view.View
+import com.example.recommendtrack.R
+import com.example.recommendtrack.databinding.FragmentArtistBinding
+import com.example.recommendtrack.presentation.ui.BaseFragment
+
+class MyArtistFragment : BaseFragment<FragmentArtistBinding>(R.layout.fragment_my_artist) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+
+
+
+}
+
--- app/src/main/java/com/example/recommendtrack/presentation/recommendtrack/RecommendTrackFragment.kt
+++ app/src/main/java/com/example/recommendtrack/presentation/recommendtrack/RecommendTrackFragment.kt
@@ -1,13 +1,11 @@
 package com.example.recommendtrack.presentation.recommendtrack
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.recommendtrack.R
 import com.example.recommendtrack.databinding.FragmentRecommendTrackBinding
 import com.example.recommendtrack.presentation.ui.BaseFragment
-import com.example.recommendtrack.presentation.ui.artist.ArtistViewModel
 import com.example.recommendtrack.presentation.ui.recommendtrack.RecommendTrackViewModel
 import timber.log.Timber
 
@@ -29,7 +27,6 @@ class RecommendTrackFragment : BaseFragment<FragmentRecommendTrackBinding>(R.lay
 
 
     companion object {
-
         fun newInstance() =
             RecommendTrackFragment().apply {
                 arguments = Bundle().apply {
