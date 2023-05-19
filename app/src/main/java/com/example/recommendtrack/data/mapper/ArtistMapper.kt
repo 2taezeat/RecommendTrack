8ff44a1d16ec3cb60b406030e2326503e03834d1
2taezeat <xorudfl772@naver.com>
2023-05-19 13:20:05 +0900

    feat: ArtistMapper dummy 추가

--- app/src/main/java/com/example/recommendtrack/data/mapper/ArtistMapper.kt
+++ app/src/main/java/com/example/recommendtrack/data/mapper/ArtistMapper.kt
@@ -1,23 +1,34 @@
 package com.example.recommendtrack.data.mapper
 
 import com.example.recommendtrack.data.dto.ArtistDto
-import com.example.recommendtrack.data.dto.TokenDto
 import com.example.recommendtrack.domain.entity.Artist
-import com.example.recommendtrack.domain.entity.Token
 import com.skydoves.sandwich.ApiResponse
 import com.skydoves.sandwich.ApiSuccessModelMapper
 
 object ArtistMapper : ApiSuccessModelMapper<ArtistDto, Artist> {
     override fun map(apiSuccessResponse: ApiResponse.Success<ArtistDto>): Artist {
-        val artistItem = apiSuccessResponse.data.artists.items[0]
-        return Artist(
-            followers = artistItem.followers.total,
-            genres = artistItem.genres,
-            id = artistItem.id,
-            imageUrl = artistItem.images[0].url,
-            name = artistItem.name,
-            popularity = artistItem.popularity
-        )
+        val items = apiSuccessResponse.data.artists.items
+        if (items.isEmpty()) {
+            return Artist(
+                followers = 0,
+                genres = listOf(),
+                id = "",
+                imageUrl = "",
+                name = "",
+                popularity = 0
+            )
+        } else {
+            val artistItem = items[0]
+            return Artist(
+                followers = artistItem.followers.total,
+                genres = artistItem.genres,
+                id = artistItem.id,
+                imageUrl = artistItem.images[0].url,
+                name = artistItem.name,
+                popularity = artistItem.popularity
+            )
+        }
+
     }
 
 }
