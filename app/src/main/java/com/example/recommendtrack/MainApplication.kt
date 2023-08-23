330a0df987664c1dae16862f55143e606c06bccb
2taezeat <xorudfl772@naver.com>
2023-08-23 15:53:10 +0900

    feat: TokenWorker 구현 및 WorkManager Configuration

--- app/src/main/java/com/example/recommendtrack/MainApplication.kt
+++ app/src/main/java/com/example/recommendtrack/MainApplication.kt
@@ -1,13 +1,34 @@
 package com.example.recommendtrack
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application() {
+class MainApplication : Application(), Configuration.Provider {
+
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
     }
+
+    
 }
+
+
+
+
---
+++ app/src/main/java/com/example/recommendtrack/presentation/worker/TokenWorker.kt
@@ -0,0 +1,59 @@
+package com.example.recommendtrack.presentation.worker
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.recommendtrack.BuildConfig
+import com.example.recommendtrack.domain.usecase.token.WorkerGetTokenUseCase
+import com.example.recommendtrack.utils.Constants
+import com.example.recommendtrack.utils.PreferenceKey
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import timber.log.Timber
+
+@HiltWorker
+class TokenWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val dataStore: DataStore<Preferences>,
+    private val workerGetTokenUseCase: WorkerGetTokenUseCase,
+) : CoroutineWorker(appContext, workerParams) {
+
+
+    override suspend fun doWork(): Result {
+        Timber.d("worker_doWork")
+        return try {
+            getToken()
+            Result.success()
+        } catch (exception: Exception) {
+            Result.failure()
+        }
+    }
+
+
+    private suspend fun getToken() {
+        Timber.d("worker_getToken_Call")
+        val tokenRemoteFlow = workerGetTokenUseCase(
+            Constants.SPOTIFY_GRANT_TYPE,
+            BuildConfig.SPOTIFY_CLIENT_ID,
+            BuildConfig.SPOTIFY_CLIENT_SECRET
+        )
+        tokenRemoteFlow.collect { token ->
+            writeTokenDataStore(token.accessToken)
+        }
+
+    }
+
+    private suspend fun writeTokenDataStore(str: String) {
+        Timber.d("worker_writeTokenDataStore_Call")
+        dataStore.edit { preferences ->
+            preferences[PreferenceKey.tokenPreferenceKey] = str
+        }
+    }
+
+
+}
